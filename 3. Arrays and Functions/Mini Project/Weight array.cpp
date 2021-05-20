ef81506cbb12872cc9f479e99337b4fdf9ebcf3e
Yunus <ymohammad2000@gmail.com>
2021-05-20 12:59:23 +0530

    commit

--- 3. Arrays and Functions/Mini Project/Weight array.cpp
+++ 3. Arrays and Functions/Mini Project/Weight array.cpp
@@ -1,15 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-	int a[][];
-	cin >> a[][];
-	int m = sizeof(a);
-	int n = sizeof(a[0]);
-	for(int i=0;i<m;i++){
-		for(int j=0;j<n;j++){
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
+void idheavy(int a[][3]){
+	int corner = a[0][0] + a[2][2] + a[0][2] + a[2][0];
+	int center = a[0][1] + a[1][0] + a[2][1] + a[1][2];
+	if(corner > center){
+		cout << 1;
 	}
+	if(corner < center){
+		cout << 2;
+	}
+	if(corner == center){
+		cout << 3;
+	}
+}
+
+int main(){
+	int a[3][3];
+	cout << "Enter 9 Values:" << endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cin >> a[i][j];
+		}
+	}
+	idheavy(a);
+
+	return 0;
 }
